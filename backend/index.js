4dab3f8fb63de2983e9e26f8e68b4b06aa0146d1
chandusiriyala <chandusiriyala116@gmail.com>
2024-09-04 04:01:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const dbPath = path.resolve('/tmp/database.db');
+const dbPath = path.resolve('/temp/database.db');
 
 
 // Initialize SQLite database
